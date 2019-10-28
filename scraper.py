b0e850b367ad08335777331301f9803246dae3d5
むいにゃん <hngrmi@gmail.com>
2019-10-28 22:55:57 +0900

    main部を実装

--- scraper.py
+++ scraper.py
@@ -4,41 +4,59 @@ import urllib
 import urllib.request
 from bs4 import BeautifulSoup
 import json
+import argparse
 
-# アクセスするURL
-url = "https://w.atwiki.jp/sp12ex-hard/pages/12.html"
+def clear():
+    pass
 
-html = urllib.request.urlopen(url)
+def hard():
+    pass
 
-soup = BeautifulSoup(html, "html.parser")
-musics = []
+def exh():
+    # アクセスするURL
+    url = "https://w.atwiki.jp/sp12ex-hard/pages/12.html"
 
-refs = soup.select("h2")
-refs = filter(lambda s: bool(s.string), refs)
-refs = filter(lambda s: re.match(r"難度(\d+|\?\?\?)", s.string), refs)
-refs = map(lambda s: (s["id"], s.string[2:]), refs)
-refs = list(refs)
+    html = urllib.request.urlopen(url)
 
-#print(refs)
+    soup = BeautifulSoup(html, "html.parser")
+    musics = []
 
-suffixes = [
-    ("[H]", "H")
-    , ("[A]", "A")
-    , ("†", "L")
-    , ("†LEGGENDARIA", "L")
-]
+    refs = soup.select("h2")
+    refs = filter(lambda s: bool(s.string), refs)
+    refs = filter(lambda s: re.match(r"難度(\d+|\?\?\?)", s.string), refs)
+    refs = map(lambda s: (s["id"], s.string[2:]), refs)
+    refs = list(refs)
 
-for ref in refs:
-    difficulty = ref[1]
-    titles = soup.select("#{} + table tr".format(ref[0]))[1:]
-    titles = map(lambda s: s.select_one("td:nth-of-type(2)").string, titles)
-    for title in titles:
-        mode = "A"
-        for suf in suffixes:
-            if title.endswith(suf[0]):
-                mode = suf[1]
-                title = title[:-1 * len(suf[0])]
-                break
-        musics.append((difficulty, title, mode))
+    #print(refs)
 
-print(json.dumps(musics, ensure_ascii=False))
+    suffixes = [
+        ("[H]", "H")
+        , ("[A]", "A")
+        , ("†", "L")
+        , ("†LEGGENDARIA", "L")
+    ]
+
+    for ref in refs:
+        difficulty = ref[1]
+        titles = soup.select("#{} + table tr".format(ref[0]))[1:]
+        titles = map(lambda s: s.select_one("td:nth-of-type(2)").string, titles)
+        for title in titles:
+            mode = "A"
+            for suf in suffixes:
+                if title.endswith(suf[0]):
+                    mode = suf[1]
+                    title = title[:-1 * len(suf[0])]
+                    break
+            musics.append((difficulty, title, mode))
+
+    print(json.dumps(musics, ensure_ascii=False))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('src', metavar='S', type=str)
+    args = parser.parse_args()
+    
+    src = args.src
+    
+    if src == "exh":
+        exh()
